89cb0023e4b96519986f6ade4b8646109bbb8dd6
AntonLovkin <anton28111987@gmail.com>
2021-08-02 23:04:01 +0300

    Update App.js
    hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Switch } from 'react-router-dom';
+import { Switch, Route  } from 'react-router-dom';
 import AppBar from './UserMenu/AppBar';
 import { lazy, Suspense, useEffect } from 'react';
 import authOperations from './redux/auth/auth-operations';
 import { useDispatch } from 'react-redux';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
+import RegisterView from './views/RegisterView';
 
 const ContactsView = lazy(() => import('./views/ContactsView'));
-const RegisterView = lazy(() => import('./views/RegisterView'));
+// const RegisterView = lazy(() => import('./views/RegisterView'));
 const LoginView = lazy(() => import('./views/LoginView'));
 
 
@@ -29,12 +30,19 @@ export default function App() {
     return (
       <>
         <AppBar />
-
+      
         <Suspense fallback={<p>Loading...</p>}>
           <Switch>
-            <PublicRoute path="/register" restricted component={RegisterView} redirectTo='/contacts'/>
-            <PublicRoute path="/login" restricted component={LoginView} redirectTo='/contacts'/>
-            <PrivateRoute path="/contacts" component={ContactsView} redirectTo='/login' />
+            <PublicRoute exact path="/register" restricted redirectTo='/contacts'>
+                <RegisterView />
+            </PublicRoute>
+            <PublicRoute exact path="/login" restricted redirectTo='/contacts'>
+                <LoginView />
+            </PublicRoute>
+            {/* <PublicRoute path="/login" restricted component={LoginView} redirectTo='/contacts'/> */}
+            <PrivateRoute path="/contacts" redirectTo='/login'>
+              <ContactsView/>
+            </PrivateRoute>
           </Switch>
         </Suspense>
       </>
